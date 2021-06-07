81714c4f9b3d495a2869e2d8a410ad54f26f066d
Tirth <84908224+tirthbhatt21@users.noreply.github.com>
2021-06-07 17:52:17 +0530

    Add files via upload

---
+++ curdwithouttheme/db/database.sql
@@ -0,0 +1,10 @@
+create database register;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (ID)
+);
---
+++ curdwithouttheme/index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+
+<body>  
+    <form method="post">
+        <table border="2" align="center">
+            <tr><td>
+            Welcome On CURD Operation...
+            </></tr>
+    <br /> <br />
+    <div>
+        <a href="login.php"> Login</a> | <a href="register.php"> Register</a>
+    </div>
+</table>
+</form>
+</body>
+
+</html>
