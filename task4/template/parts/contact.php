fa2b2c344d8a25ba046a6a6da67566e98596c87b
Tirth <84908224+tirthbhatt21@users.noreply.github.com>
2021-06-07 17:48:20 +0530

    Add files via upload

---
+++ task4/template/parts/contact.php
@@ -0,0 +1,91 @@
+<head>
+
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Start | Landing Page for Startup and SaaS Business</title>
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--====== Line Icons css ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+
+<section id="contact" class="contact-area">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="section-title text-center pb-10">
+                        <h4 class="title">Get In touch</h4>
+                        <p class="text">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
+                    </div> <!-- section title -->
+                </div>
+            </div> <!-- row -->
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="contact-form">
+                        <form id="contact-form" action="assets/contact.php" method="post" data-toggle="validator">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="single-form form-group">
+                                        <input type="text" name="name" placeholder="Your Name" data-error="Name is required." required="required">
+                                        <div class="help-block with-errors"></div>
+                                    </div> <!-- single form -->
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="single-form form-group">
+                                        <input type="email" name="email" placeholder="Your Email" data-error="Valid email is required." required="required">
+                                        <div class="help-block with-errors"></div>
+                                    </div> <!-- single form -->
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="single-form form-group">
+                                        <input type="text" name="subject" placeholder="Subject" data-error="Subject is required." required="required">
+                                        <div class="help-block with-errors"></div>
+                                    </div> <!-- single form -->
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="single-form form-group">
+                                        <input type="text" name="phone" placeholder="Phone" data-error="Phone is required." required="required">
+                                        <div class="help-block with-errors"></div>
+                                    </div> <!-- single form -->
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="single-form form-group">
+                                        <textarea placeholder="Your Mesaage" name="message" data-error="Please, leave us a message." required="required"></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div> <!-- single form -->
+                                </div>
+                                <p class="form-message"></p>
+                                <div class="col-md-12">
+                                    <div class="single-form form-group text-center">
+                                        <button type="submit" class="main-btn">send message</button>
+                                    </div> <!-- single form -->
+                                </div>
+                            </div> <!-- row -->
+                        </form>
+                    </div> <!-- row -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- conteiner -->
+    </section>
